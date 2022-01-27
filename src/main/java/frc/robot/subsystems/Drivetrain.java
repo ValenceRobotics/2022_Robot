54750db4346ab099893f02eed8b755c9712c222d
D123aniel <zhang23d@ncssm.edu>
2022-01-26 21:24:31 -0500

    drive train example

---
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -0,0 +1,11 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class Drivetrain extends SubsystemBase{
+    private final WPI_TalonSRX m_leftFront = new WPI_TalonSRX(Constants.Drivetrain).kLeftFront);
+    private
+}
