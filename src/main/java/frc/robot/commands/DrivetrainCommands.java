d523143c5f520bf787e59aedeb5d1b333cf33a16
alice15311 <alice15311@gmail.com>
2022-03-21 23:19:37 -0400

    squared drive inputs

--- src/main/java/frc/robot/commands/DrivetrainCommands.java
+++ src/main/java/frc/robot/commands/DrivetrainCommands.java
@@ -1,5 +1,7 @@
 package frc.robot.commands;
 
+import org.opencv.core.Mat;
+
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
@@ -8,15 +10,15 @@ import frc.robot.subsystems.DrivetrainSubsystem;
 
 public class DrivetrainCommands {
     public static Command tankDriveXboxController(DrivetrainSubsystem drivetrain, XboxController controller) {
-        return new RunCommand(() -> drivetrain.tankDrive(-controller.getLeftY(), -controller.getRightY()), drivetrain);
+        return new RunCommand(() -> drivetrain.tankDrive(-Math.pow(controller.getLeftY(), 2), -Math.pow(controller.getRightY(), 2)), drivetrain);
     }
 
     public static Command arcadeDriveXboxController(DrivetrainSubsystem drivetrain, XboxController controller) {
-        return new RunCommand(() -> drivetrain.arcadeDrive(-controller.getLeftY(), controller.getRightX()), drivetrain);
+        return new RunCommand(() -> drivetrain.arcadeDrive(-(controller.getLeftY()*Math.abs(controller.getLeftY())), controller.getRightX()*Math.abs(controller.getRightX())), drivetrain);
     }
 
     public static Command arcadeDriveXboxControllerBackwards(DrivetrainSubsystem drivetrain, XboxController controller) {
-        return new RunCommand(() -> drivetrain.arcadeDrive(controller.getLeftY(), controller.getRightX()), drivetrain);
+        return new RunCommand(() -> drivetrain.arcadeDrive(controller.getLeftY()*Math.abs(controller.getLeftY()), controller.getRightX()*Math.abs(controller.getRightX())), drivetrain);
     }
 
 
